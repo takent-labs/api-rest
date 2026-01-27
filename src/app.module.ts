60b405188cfff7292d86be83249cec2219b5f4d2
oskirove <oskirove@gmail.com>
2026-01-27 21:07:28 +0100

    chore: corregir estructura de carpetas y configuración de módulos ESM
    Se movió PrismaService a la carpeta src (como indica la doc) y se ajustaron las rutas de importación del cliente generado para resolver errores de ejecución en Node.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaService } from 'prisma.service';
+import { Module } from "@nestjs/common";
+import { PrismaService } from "./prisma.service.js";
+import { AppService } from "./app.service.js";
+import { AppController } from "./app.controller.js";
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
-export class AppModule {}
+export class AppModule { }
