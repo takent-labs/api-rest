163c4c4612623aff83cfea8f1a90a72fd05a5548
oskirove <oskirove@gmail.com>
2026-01-31 21:02:04 +0100

    feat: activada la validación global de los DTO
    He habilitado 'whitelist' para ignorar campos no definidos y 'forbidNonWhitelisted' para rechazar peticiones con datos extra, mejorando la seguridad. Además, he activado 'transform' para el mapeo automático de tipos.

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,31 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsOptional, IsString, IsUrl, IsUUID } from "class-validator";
+import type { UUID } from "crypto";
+
+export class CreateUserDto {
+    @IsUUID()
+    id: UUID;
+
+    @IsString()
+    @IsNotEmpty()
+    username: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsOptional()
+    firstName: string;
+
+    @IsString()
+    @IsOptional()
+    lastName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    passwordHash: string;
+
+    @IsString()
+    @IsUrl()
+    imageUrl: string;
+}
