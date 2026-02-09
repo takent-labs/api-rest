344c459154ab05a36affbbc927ce2b59db67df1e
oskirove <oskirove@gmail.com>
2026-02-09 01:05:03 +0100

    feat(User): implementada ruta de perfil

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,29 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service.js';
-import { CreateUserDto } from './dto/create-user.dto.js';
-import { UpdateUserDto } from './dto/update-user.dto.js';
+import { AuthGuard } from '../auth/guards/auth.guard.js';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
-  @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
-    return await this.usersService.create(createUserDto);
+  @Get('data')
+  @UseGuards(AuthGuard)
+  async getData(@Request() req) {
+    const userId = req.user.sub;
+    return await this.usersService.findOne(userId);
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.usersService.findOne(id);
-  }
-
-  @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return await this.usersService.update(id, updateUserDto);
-  }
-
-  @Delete(':id')
-  async remove(@Param('id') id: string) {
-    return await this.usersService.remove(id);
   }
 }
